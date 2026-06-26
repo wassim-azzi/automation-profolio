e8f3581ac62f51d31d563b6525a4a6b6feeec366
wassim-azzi <wassimazzi30@gmail.com>
2026-06-27 00:55:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,15 +23,3 @@ sections.forEach(section => {
     observer.observe(section);
 
 });
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-if(menuToggle){
-
-    menuToggle.addEventListener("click",()=>{
-
-        navLinks.classList.toggle("active");
-
-    });
-
-}
