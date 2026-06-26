671973e90a224a66174534c600b5d317d8e94022
wassim-azzi <wassimazzi30@gmail.com>
2026-06-26 23:41:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const sections = document.querySelectorAll("section");
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+
+    threshold:0.15
+
+});
+
+sections.forEach(section => {
+
+    observer.observe(section);
+
+});
